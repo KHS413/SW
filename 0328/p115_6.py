65d48ba68075cfe70ef6a173243382dde3834d9b
KHS413 <127271988+KHS413@users.noreply.github.com>
2023-04-04 13:15:22 +0900

    Add files via upload

---
+++ 0328/p115_6.py
@@ -0,0 +1,17 @@
+'''
+2023.03.28
+김희수
+47과 18의 가감승제 구하는 프로그램
+'''
+su1=47
+su2=18
+
+# res1=num1+num2 #res1 변수에 num1*num2결과 저장
+# res2=num1-num2 #res2 변수에 num1/num2결과 저장
+# res3=num1*num2 #res3 변수에 num//num2결과 저장
+# res4=num1/num2 #res4 변수에 num1%num2결과 저장
+
+print(su1,"과",su2,"의 합은 :",su1+su2)
+print(su1,"과",su2,"의 차는 :",su1-su2)
+print(su1,"과",su2,"의 곱은 :",su1*su2)
+print(su1,"과",su2,"의 나눗셈은 :",su1/su2)
---
+++ 0328/p116_12.py
@@ -0,0 +1,13 @@
+'''
+2023.03.28
+김희수
+윗변이23cm 아랫변이34cm,높이가13cm인 사다리꼴의 넓이를 구하기
+'''
+
+on1=23
+bottom1=34
+height1=13
+
+area= ((on1+bottom1) * height1 ) / 2
+
+print("윗변이",on1,"밑변이",bottom1,"높이가",height1,"인 사다리꼴의 넓이는:",area)
