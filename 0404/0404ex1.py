d7df8e6060211ecc16ec6ba9251010db89ea590e
KHS413 <127271988+KHS413@users.noreply.github.com>
2023-04-04 13:14:09 +0900

    Add files via upload

---
+++ 0404/0404ex1.py
@@ -0,0 +1,18 @@
+'''
+2023.04.04
+김희수
+표준출력(print)연습
+'''
+
+print('한국','korea',2023)
+print('한국','korea',2023,sep='-') # 구분자(-)넣어서 출력
+print('한국','korea',2023,end='@')#문장끝 @출력
+print("\n") #한줄띄우기
+print('python','language');print("Hi python")
+print('python','language',end=' ');print("Hi python") #줄 바꿈 없이 출력
+print("Hi python")
+print("'Hi python'") #작은 따옴표 출력
+print("\'Hi pyhton\'") #작은 따옴표 출력
+print("python "*5)
+print("hi \n python") #\n은 줄바꿈
+print("\n 포맷 활용")
